d3f89578e374f97fb08e9a4206fc67da35ff4c98
Helena de Aldeaseca <helenam1984@gmail.com>
2026-05-20 22:41:56 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    TanStackRouterVite({ 
+      autoCodeSplitting: true,
+      routesDirectory: "./src/routes",
+      generatedRouteTree: "./src/routeTree.gen.ts",
+      disableManifestGeneration: true,
+    }),
     react(),
     tailwindcss(),
     tsconfigPaths(),
