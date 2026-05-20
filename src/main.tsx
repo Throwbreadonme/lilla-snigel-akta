e7b07f2d9b1eb4f326cd2fba90324da3644949e8
Helena de Aldeaseca <helenam1984@gmail.com>
2026-05-20 16:47:06 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createRouter, createRoute, createRootRoute } from "@tanstack/react-router";
+import { RouterProvider, createRouter, createRoute, createRootRoute, Outlet } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Index } from "./routes/index";
+import { Library } from "./routes/library";
+import { TomtenOchSkogen } from "./routes/library/tomten-och-skogen";
 import "./styles.css";
 
 const queryClient = new QueryClient();
 
-// Import route components
-import { Index } from "./routes/index";
-import { Library } from "./routes/library";
-import { TomtenOchSkogen } from "./routes/library/tomten-och-skogen";
-
 const rootRoute = createRootRoute({
-  component: () => {
-    return (
-      <QueryClientProvider client={queryClient}>
-        <Outlet />
-      </QueryClientProvider>
-    );
-  },
+  component: () => (
+    <QueryClientProvider client={queryClient}>
+      <Outlet />
+    </QueryClientProvider>
+  ),
 });
 
 const indexRoute = createRoute({
