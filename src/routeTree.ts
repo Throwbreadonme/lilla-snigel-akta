6c5c8d3a4edc224fa51384c49347c6b911e5adec
Helena de Aldeaseca <helenam1984@gmail.com>
2026-05-21 07:47:25 +0200

    Update routeTree.ts

--- src/routeTree.ts
+++ src/routeTree.ts
@@ -2,11 +2,13 @@ import { Route as rootRouteImport } from './routes/__root'
 import { Route as IndexRouteImport } from './routes/index'
 import { Route as LibraryRouteImport } from './routes/library'
 import { Route as TomtenRouteImport } from './routes/library/tomten-och-skogen'
+import { Route as AboutRouteImport } from './routes/about'
 
 const IndexRoute = IndexRouteImport.update({ id: '/', path: '/', getParentRoute: () => rootRouteImport } as any)
 const LibraryRoute = LibraryRouteImport.update({ id: '/library', path: '/library', getParentRoute: () => rootRouteImport } as any)
 const TomtenRoute = TomtenRouteImport.update({ id: '/library/tomten-och-skogen', path: '/library/tomten-och-skogen', getParentRoute: () => rootRouteImport } as any)
+const AboutRoute = AboutRouteImport.update({ id: '/about', path: '/about', getParentRoute: () => rootRouteImport } as any)
 
-const rootRouteChildren = { IndexRoute, LibraryRoute, TomtenRoute }
+const rootRouteChildren = { IndexRoute, LibraryRoute, TomtenRoute, AboutRoute }
 
 export const routeTree = rootRouteImport._addFileChildren(rootRouteChildren)
