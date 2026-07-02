302fb4848c16e48cdd2ad1cbcac62f6d1fe5a427
Helena de Aldeaseca <helenam1984@gmail.com>
2026-07-02 08:40:04 +0200

    Update routeTree.ts
    Update including Marina i simhallen story

--- src/routeTree.ts
+++ src/routeTree.ts
@@ -2,15 +2,17 @@ import { Route as rootRouteImport } from './routes/__root'
 import { Route as IndexRouteImport } from './routes/index'
 import { Route as LibraryRouteImport } from './routes/library'
 import { Route as TomtenRouteImport } from './routes/library/tomten-och-skogen'
+import { Route as MarinaRouteImport } from './routes/library/marina-i-simhallen'
 import { Route as AboutRouteImport } from './routes/about'
 import { Route as FAQRouteImport } from './routes/faq'
 
 const IndexRoute = IndexRouteImport.update({ id: '/', path: '/', getParentRoute: () => rootRouteImport } as any)
 const LibraryRoute = LibraryRouteImport.update({ id: '/library', path: '/library', getParentRoute: () => rootRouteImport } as any)
 const TomtenRoute = TomtenRouteImport.update({ id: '/library/tomten-och-skogen', path: '/library/tomten-och-skogen', getParentRoute: () => rootRouteImport } as any)
+const MarinaRoute = MarinaRouteImport.update({ id: '/library/marina-i-simhallen', path: '/library/marina-i-simhallen', getParentRoute: () => rootRouteImport } as any)
 const AboutRoute = AboutRouteImport.update({ id: '/about', path: '/about', getParentRoute: () => rootRouteImport } as any)
 const FAQRoute = FAQRouteImport.update({ id: '/faq', path: '/faq', getParentRoute: () => rootRouteImport } as any)
 
-const rootRouteChildren = { IndexRoute, LibraryRoute, TomtenRoute, AboutRoute, FAQRoute }
+const rootRouteChildren = { IndexRoute, LibraryRoute, TomtenRoute, MarinaRoute, AboutRoute, FAQRoute }
 
 export const routeTree = rootRouteImport._addFileChildren(rootRouteChildren)
